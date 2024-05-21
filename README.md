c1b620048f5c2e0e930824bf156261492b957bcb
Franciele Bernardo <francielebernardo88@hotmail.com>
2024-05-21 11:06:49 -0300

    feat: pastas do projeto

--- README.md
+++
@@ -1,1 +0,0 @@
-# vue-laravel-CRUD
---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Headers title="Contatos"/>
+  <router-view></router-view>
+</template>
+
+<script>
+import Headers from './components/Headers.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Headers
+  }
+}
+</script>
+
+<style>
+
+</style>
